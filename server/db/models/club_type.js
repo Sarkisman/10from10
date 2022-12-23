402662cddf16e99f89fd10a9a72411fc143dd793
Victoria <viktoriya.zapisochnaya@gmail.com>
2022-12-23 18:29:34 +0300

    dbIsDone

---
+++ server/db/models/club_type.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Club_Type extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Club_Type.init({
+    club_id: DataTypes.INTEGER,
+    type_id: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Club_Type',
+  });
+  return Club_Type;
+};
