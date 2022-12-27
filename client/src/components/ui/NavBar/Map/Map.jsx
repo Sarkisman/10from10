c9f07560108fd23f33387fb676c94e613c677bcf
Sarkis <sarkis_strelok@mail.ru>
2022-12-27 14:52:47 +0300

    some fixes

--- client/src/components/ui/NavBar/Map/Map.jsx
+++ client/src/components/ui/NavBar/Map/Map.jsx
@@ -5,15 +5,16 @@ import './style.css';
 
 export default function Map() {
   const [map, setMap] = useState(null);
+  const clubs = useSelector((store) => store.clubs);
+  console.log(clubs);
   const dispatch = useDispatch();
   const { ymaps } = window;
   const center = [55.76, 37.64];
   function init() {
     const myMap = new ymaps.Map('map-test', {
       center,
-      zoom: 16,
+      zoom: 10,
     });
-
     // myMap.controls.remove('geolocationControl'); // удаляем геолокацию
     // myMap.controls.remove('searchControl'); // удаляем поиск
     myMap.controls.remove('trafficControl'); // удаляем контроль трафика
@@ -22,49 +23,47 @@ export default function Map() {
     myMap.controls.remove('zoomControl'); // удаляем контрол зуммирования
     myMap.controls.remove('rulerControl'); // удаляем контрол правил
     // myMap.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
-
     // placemark1.balloon.open();                            EXAMPLE
-
     dispatch(getAllClubs());
     setMap(myMap);
     return myMap;
   }
-  const clubs = useSelector((store) => store.clubs);
-  // console.log(clubs);
+
   useEffect(() => {
     ymaps.ready(init);
   }, []);
 
-  const myGeocoder = ymaps?.geocode('Клин');
-  myGeocoder?.then(
-    (res) => {
-      console.log(`Координаты объекта :${res.geoObjects.get(0).geometry.getCoordinates()}`);
-    },
-    (err) => {
-      (console.log(err));
-    },
-  );
+  // const myGeocoder = ymaps?.geocode('Клин');
+  // myGeocoder?.then(
+  //   (res) => {
+  //     console.log(`Координаты объекта :${res.geoObjects.get(0).geometry.getCoordinates()}`);
+  //   },
+  //   (err) => {
+  //     (console.log(err));
+  //   },
+  // );
   useEffect(() => {
-    // console.log(clubs, 'CLUUUUBBBBSSSS');
-    clubs?.forEach((el) => {
-      const coordinates = [el.latitude, el.longitude];
-      // console.log('coordinates', coordinates);
-      const myPlacemarkWithContent = new ymaps.Placemark(coordinates, {
-        balloonContent: `
+    setTimeout(() => {
+      clubs?.forEach((el) => {
+        const coordinates = [el.latitude, el.longitude];
+        // console.log('coordinates', coordinates);
+        const myPlacemarkWithContent = new ymaps.Placemark(coordinates, {
+          balloonContent: `
                 <div class="balloon">
                   <div class="balloon__title">${el.name}</div>
                   <button type="button" class="btn sixth" id=${el.id}>Подробнее</button>
                 </div>
                 `,
-      }, {
-        iconLayout: 'default#imageWithContent', // Необходимо указать данный тип макета.
-        iconImageHref: 'https://cdn-icons-png.flaticon.com/512/1016/1016056.png', // Своё изображение иконки метки.
-        iconImageSize: [40, 40], // Размеры метки.
-        iconImageOffset: [-24, -24], // Смещение левого верхнего угла иконки относительно, её "ножки"
-        iconContentOffset: [15, 15], // Смещение слоя с содержимым относительно слоя с картинкой.
+        }, {
+          iconLayout: 'default#imageWithContent', // Необходимо указать данный тип макета.
+          iconImageHref: 'https://cdn-icons-png.flaticon.com/512/1016/1016056.png', // Своё изображение иконки метки.
+          iconImageSize: [40, 40], // Размеры метки.
+          iconImageOffset: [-24, -24], // Смещение левого верхнего угла иконки относительно, её "ножки"
+          iconContentOffset: [15, 15], // Смещение слоя с содержимым относительно слоя с картинкой.
+        });
+        map?.geoObjects.add(myPlacemarkWithContent);
       });
-      map?.geoObjects.add(myPlacemarkWithContent);
-    });
+    }, 250);
   }, [clubs]);
 
   // const myGeocoder = ymaps.geocode('Клин');
