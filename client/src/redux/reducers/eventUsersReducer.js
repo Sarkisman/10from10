4a0970ed17f3223c2545b9fef3d36d9698c5e341
Sarkis <sarkis_strelok@mail.ru>
2023-01-04 21:38:58 +0300

    fixDelFromEvent

--- client/src/redux/reducers/eventUsersReducer.js
+++ client/src/redux/reducers/eventUsersReducer.js
@@ -6,14 +6,9 @@ export default function eventUsersReducer(state = [], action) {
     case SET_EVENT_USERS:
       return payload;
     case ADD_EVENT_USER:
-      // console.log('payload', payload);
       return [payload, ...state];
     case DELETE_EVENT_USER:
-      // console.log('payload', payload);
-      // return [payload, ...state];
-      // return state.filter((el) => el.id !== payload); // payload ===  user.id
-      return state.filter((el) => el.id !== 10); // payload ===  user.id
-
+      return state.filter((el) => el.id !== payload);
     default:
       return state;
   }
