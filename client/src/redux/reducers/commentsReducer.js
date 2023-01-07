2be8a05050df9eb15740f12e0a9c3ca392b53217
Victoria <viktoriya.zapisochnaya@gmail.com>
2023-01-07 21:14:31 +0300

    SomeSmallChanges

--- client/src/redux/reducers/commentsReducer.js
+++ client/src/redux/reducers/commentsReducer.js
@@ -6,13 +6,13 @@ export default function commentsReducer(state = [], action) {
   const { type, payload } = action;
   switch (type) {
     case SET_COMMENTS:
-      return payload; // payload == allPosts
+      return payload; // payload == allComments
     case ADD_COMMENT:
-      return [payload, ...state]; // payload === newPost
+      return [payload, ...state]; // payload === newComment
     case DELETE_COMMENT:
       return state.filter((post) => post.id !== payload); // payload === id
     case EDIT_COMMENT:
-      return state.map((post) => (post.id === payload.id ? payload : post)); // payload === updatedPost
+      return state.map((post) => (post.id === payload.id ? payload : post)); // payload === updatedComment
 
     default:
       return state;
