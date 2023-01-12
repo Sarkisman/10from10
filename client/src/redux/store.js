0184618006595e47fd1062a34d969cabe2bf6264
Victoria <viktoriya.zapisochnaya@gmail.com>
2023-01-12 16:01:08 +0300

    SomeRefactoring

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -13,7 +13,6 @@ import commentsReducer from './reducers/commentsReducer';
 import eventPhotoReducer from './reducers/eventPhotoReducer';
 import selectedClubIdReducer from './reducers/selectedClubIdReducer';
 import userSuggestedEventsReducer from './reducers/userSuggestedEventsReducer';
-import mySuggestedEventsReducer from './reducers/mySuggestedEventsReducer';
 
 const store = configureStore({
   reducer: {
@@ -30,7 +29,6 @@ const store = configureStore({
     photos: eventPhotoReducer,
     selectedClubId: selectedClubIdReducer,
     userSuggestedEvents: userSuggestedEventsReducer,
-    mySuggestedEvents: mySuggestedEventsReducer,
   },
 });
 
