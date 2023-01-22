624af301d38eb21d3d69123c7de287af374fdc8c
Sarkis <sarkis_strelok@mail.ru>
2023-01-22 21:15:07 +0300

    deployTest21

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 // const multer = require('multer'); // мультер
+const path = require('path');
 const cors = require('cors');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
@@ -22,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use(express.static('images'));
 app.use('/', express.static('/build'));
+app.use('/static', express.static(path.join(__dirname, 'public')));
 app.use(session({
   name: 'user_sid',
   secret: process.env.SESSION_SECRET ?? 'test',
