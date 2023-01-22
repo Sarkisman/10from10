b46a26039d21af161f27ca0edce250bb713027dd
Sarkis <sarkis_strelok@mail.ru>
2023-01-22 13:49:42 +0300

    deployTest14

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use(express.static('images'));
-app.use('/', express.static('./client/build'));
+app.use('/', express.static('../client/build'));
 app.use(session({
   name: 'user_sid',
   secret: process.env.SESSION_SECRET ?? 'test',
